b68a2c7cb92f632c4bfa474131ee2935da14ff23
Riku Brandt <brandtriku@gmail.com>
2022-01-28 22:09:08 +0200

    HuffmanTreeNode added

--- src/huffman.py
+++ src/huffman.py
@@ -1,4 +1,4 @@
-
+from collections import defaultdict
 class HuffmanEncoding:
     def __init__(self, file):
         self.file = file
@@ -14,18 +14,41 @@ class HuffmanEncoding:
                     return
 
     def calculate_frequency(self):
-        frequency = {}
+        frequency = defaultdict(int)
 
         iterator = self.file_reader()
         while True:
             try:
                 c = next(iterator)
-                #Adds a new key with value 1 or increments it.
-                frequency[c] = frequency.get(c, 0) +1
+                frequency[c] += 1
             except StopIteration:
                 break
 
         return dict(sorted(frequency.items(), key=lambda item: item[1], reverse=True))
+
+
+    def generate_huffman_tree(self, dict):
+        pass
+
+    def generate_file(self):
+        pass
+
+
+
+
+class HuffmanTreeNode():
+    def __init__(self, char, freq=None, left=None, right=None):
+        char = self.char
+        freq = self.freq
+        left = self.left
+        right = self.right
+
+    def set_right_node(self, right):
+        self.right = right
+
+    def set_left_node(self, left):
+        self.left = left
+
 
 if __name__ == "__main__":
     h = HuffmanEncoding("text.txt")
