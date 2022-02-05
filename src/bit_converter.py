1b388a18d81cb1185ce58497cd226eb3e0a75490
Riku Brandt <brandtriku@gmail.com>
2022-02-05 23:07:32 +0200

    Func to add additional bits for conversion

--- src/bit_converter.py
+++ src/bit_converter.py
@@ -1,11 +1,18 @@
 
+# Needed to get the length divided by 8
+def add_additional_bits(bits):
+    over = 8 - (len(bits) % 8)
+    count = format(over, "08b")
+    additional_bits = count + over * "0"
+    return additional_bits
 
 def bits_to_bytes(bits):
     
     bytes = bytearray()
-    
+    bits = add_additional_bits(bits) + bits
     for b in range(0, len(bits), 8):
         byte = (bits[b:b+8])
         bytes.append(int(byte, 2))
 
     return bytes(bytes)
+
