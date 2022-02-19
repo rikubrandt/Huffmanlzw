7a38ba485152ea184b5933788d70de95e0c3d0f7
Riku Brandt <brandtriku@gmail.com>
2022-02-20 00:00:54 +0200

    Tests for huffman coding

--- readme.md
+++ readme.md
@@ -8,3 +8,4 @@
 [Week 2](https://github.com/rikubrandt/Huffmanlzw/blob/main/documentation/week2_report.md)
 [Week 3](https://github.com/rikubrandt/Huffmanlzw/blob/main/documentation/week3_report.md)
 [Week 4](https://github.com/rikubrandt/Huffmanlzw/blob/main/documentation/week4_report.md)
+[Week 5](https://github.com/rikubrandt/Huffmanlzw/blob/main/documentation/week5_report.md)
--- src/tests/huffman_test.py
+++ src/tests/huffman_test.py
@@ -7,11 +7,30 @@ class TestHuffman(unittest.TestCase):
     def setUp(self):
         self.huffman = HuffmanCoding()
         self.test_text = "This a test text hello 123."
-
+        self.frequency = {' ': 5, 't': 4, 'e': 3, 'h': 2, 's': 2, 'l': 2, 'T': 1, 'i': 1, 'a': 1, 'x': 1, 'o': 1, '1': 1, '2': 1, '3': 1, '.': 1}
+        self.tree = self.huffman.generate_huffman_tree(self.frequency)
+        self.codes = {' ': '00', 'e': '010', 'a': '0110', 'l': '0111', 't': '100', '2': '10100', 'i': '10101', 's': '1011', 'T': '11000', '1': '11001', 'h': '1101', '3': '11100', '.': '11101', 'x': '11110', 'o': '11111'}
+        self.encoded = "110001101101011011000110001000101011100001000101111010000110101001110111111110011001101001110011101"
+        self.encoded_tree = "00100100000010110010101011000011011011000010111010000100110010101101001101110011000101010100100110001101101000001001100111001011100101111000101101111"
 
     def test_calculate_frequency(self):
         freq = self.huffman.calculate_frequency(self.test_text)
-        self.assertEqual(freq, {' ': 5, 't': 4, 'e': 3, 'h': 2, 's': 2, 'l': 2, 'T': 1, 'i': 1, 'a': 1, 'x': 1, 'o': 1, '1': 1, '2': 1, '3': 1, '.': 1}
-)
+        self.assertEqual(freq, self.frequency)
 
+    def test_generate_huffman_tree(self):
+        tree = self.huffman.generate_huffman_tree(self.frequency)
+        self.assertEqual(tree.char, None)
+
+    def test_generate_codes(self):
+        table = {}
+        self.huffman.generate_codes(self.tree, "", table)
+        self.assertEqual(table, self.codes)
+
+    def test_generate_encoded_text(self):
+        encoded_text = self.huffman.generate_encoded_text(self.codes, self.test_text)
+        self.assertEqual(encoded_text, self.encoded)
+
+    def test_tree_to_bits(self):
+        bits = self.huffman.huffman_tree_to_bits(self.tree)
+        self.assertEqual(bits, self.encoded_tree)
     
